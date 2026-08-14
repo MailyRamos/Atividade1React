6561208d155ee811cb700b22e0e9d31d52399d49
MailyRamos <maily.ramos@aluno.senai.br>
2026-08-14 11:33:29 -0300

    Exercício 1 concluído
    Finalizada

---
+++ variaveis/src/App.jsx
@@ -0,0 +1,20 @@
+import './App.css'
+
+function App() {
+  // Declaração de variáveis
+  const nome = "Nintendo Switch"
+  const marca = "Nintendo"
+  const preco = "R$2.100"
+
+  return (
+    <>
+    <h1>Dados do Produto</h1>
+    <h3>Nome: {nome}</h3>
+    <p>Console de Video Game Portátil Híbrido</p>
+    <h3>Marca: {marca}</h3>
+    <h3>Preço: {preco}</h3>
+    </>
+  )
+}
+
+export default App
